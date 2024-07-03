4ab8f8d725f56c3cd1ac2224ec9429da0adb60a2
Sean <Kwasnicki.Sean@gmail.com>
2024-07-03 15:06:14 -0700

    debugging

--- app/api/yahoo_finance_client.py
+++ app/api/yahoo_finance_client.py
@@ -9,24 +9,46 @@ load_dotenv()
 API_KEY = os.getenv('ALPHA_VANTAGE_API_KEY')
 BASE_URL = 'https://www.alphavantage.co/query'
 
+# def get_historical_prices(symbol):
+#     stock = yf.Ticker(symbol)
+#     end_date = datetime.now()
+#     start_date = end_date - timedelta(days=5)
+#     historical_data = stock.history(start=start_date, end=end_date)
+#     historical_prices = historical_data.to_dict('index')
+#     formatted_data = {}
+
+#     for date, data in historical_prices.items():
+#         formatted_data[date.strftime('%Y-%m-%d')] = {
+#             '1. open': data['Open'],
+#             '4. close': data['Close'],
+#             '2. high': data['High'],
+#             '3. low': data['Low'],
+#             '5. volume': data['Volume']
+#         }
+
+#     return formatted_data
+
 def get_historical_prices(symbol):
     stock = yf.Ticker(symbol)
-    end_date = datetime.now()
-    start_date = end_date - timedelta(days=5)
-    historical_data = stock.history(start=start_date, end=end_date)
-    historical_prices = historical_data.to_dict('index')
-    formatted_data = {}
+    end_date = datetime.now().strftime('%Y-%m-%d')
+    start_date = (datetime.now() - timedelta(days=365)).strftime('%Y-%m-%d')
 
-    for date, data in historical_prices.items():
-        formatted_data[date.strftime('%Y-%m-%d')] = {
-            '1. open': data['Open'],
-            '4. close': data['Close'],
-            '2. high': data['High'],
-            '3. low': data['Low'],
-            '5. volume': data['Volume']
-        }
+    try:
+        historical_data = stock.history(start=start_date, end=end_date)
+        if not historical_data.empty:
+            historical_data.index = pd.to_datetime(historical_data.index)
+            if hasattr(historical_data.index, 'tz_localize'):
+                historical_data.index = historical_data.index.tz_localize("UTC").tz_convert("America/New_York")
 
-    return formatted_data
+            prices = historical_data['Close'].tolist()
+            dates = historical_data.index.strftime('%Y-%m-%d').tolist()
+            return [{'date': date, 'close_price': price} for date, price in zip(dates, prices)]
+        else:
+            print(f"No historical data found for {symbol}")
+            return None
+    except Exception as e:
+        print(f"Error fetching historical prices for {symbol}: {e}")
+        return None
 
 def get_stock_details(symbol):
     try:
