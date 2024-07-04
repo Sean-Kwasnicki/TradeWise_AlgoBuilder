95fb6e699c7408fb9c3aaa0e30a7a8b93885388f
Sean <Kwasnicki.Sean@gmail.com>
2024-07-03 17:27:09 -0700

    adding newsdata api

--- app/api/newsdata_client.py
+++ app/api/newsdata_client.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 NEWS_API_KEY = os.getenv('NEWS_API_KEY')
-api = NewsDataApiClient(apikey=NEWS_API_KEY)
+api = NewsDataApiClient(apikey='pub_3448302204e26f6fb35ea5a80f8c03de5682a')
 
 def get_news_by_company_name(company_name):
     response = api.news_api(q=company_name, language="en")
