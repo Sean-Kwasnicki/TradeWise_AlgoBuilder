c62f000570a5c953b8836111d44c54f50e54fe05
Sean <Kwasnicki.Sean@gmail.com>
2024-07-19 21:44:11 -0700

    fixing error

--- react-vite/src/components/StockDetails/StockDetailA.jsx
+++ react-vite/src/components/StockDetails/StockDetailA.jsx
@@ -1,22 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchStock } from '../../redux/stock';
-import { getAllPortfoliosThunk } from '../../redux/portfolio';
-import { getAllWatchlistsThunk } from '../../redux/watchlist';
+import { addPortfolioStockThunk, getAllPortfoliosThunk } from '../../redux/portfolio';
+import { addWatchlistStockThunk, getAllWatchlistsThunk } from '../../redux/watchlist';
 import { FaSpinner } from 'react-icons/fa';
 import TradingViewMiniWidget from '../SmartChart/TradingViewMiniWidget';
+import './StockDetail.css';
 import AddToPortfolioModal from '../Research/AddToPortfolioModal';
 import AddToWatchlistModal from '../Research/AddToWatchlistModal';
 import { useModal } from '../../context/Modal';
-import './StockDetail.css';
+
 
 const StockDetailA = ({ symbol, detailType, isWinner }) => {
     const [loading, setLoading] = useState(false);
     const [visibleCharts, setVisibleCharts] = useState({});
     const dispatch = useDispatch();
     const stockA = useSelector((state) => state.stocks.stocks && state.stocks.stocks[symbol]);
+    const portfolios = useSelector((state) => state.portfolio.portfolios);
+    const watchlists = useSelector((state) => state.watchlist.watchlists);
     const user = useSelector((state) => state.session.user);
-    const { setModalContent, closeModal } = useModal();
+    const { setModalContent } = useModal();
 
     useEffect(() => {
         if (symbol) {
@@ -29,15 +32,15 @@ const StockDetailA = ({ symbol, detailType, isWinner }) => {
         }
     }, [dispatch, symbol]);
 
-    const handleAddToPortfolio = async () => {
-        dispatch(getAllPortfoliosThunk());
-        setModalContent(<AddToPortfolioModal symbol={symbol} closeModal={closeModal} />);
-    };
 
-    const handleAddToWatchlist = async () => {
-        dispatch(getAllWatchlistsThunk());
-        setModalContent(<AddToWatchlistModal symbol={symbol} currentPrice={stockA.current_price} closeModal={closeModal} />);
-    };
+    const handleAddToPortfolio = () => {
+        setModalContent(<AddToPortfolioModal symbol={symbol} />);
+      };
+
+      const handleAddToWatchlist = () => {
+        setModalContent(<AddToWatchlistModal symbol={symbol} currentPrice={stockA.current_price} />);
+      };
+
 
     const toggleChartVisibility = (symbol) => {
         setVisibleCharts((prev) => ({
@@ -57,9 +60,9 @@ const StockDetailA = ({ symbol, detailType, isWinner }) => {
                 <div>
                     <h2>Stock Details for {stockA.name}: {stockA.symbol}</h2>
                     <p>Current Price: ${stockA.current_price}</p>
-                    {/* <p>Market Cap: ${stockA.market_cap}</p>
-                    <p>PE Ratio: {stockA.pe_ratio}</p>
-                    <p>Dividend Yield: {stockA.dividend_yield}</p> */}
+                    {/* <p>Market Cap: ${stockB.market_cap}</p>
+                    <p>PE Ratio: {stockB.pe_ratio}</p>
+                    <p>Dividend Yield: {stockB.dividend_yield}</p> */}
                     <p>52 Week High: ${stockA.week_52_high}</p>
                     <p>52 Week Low: ${stockA.week_52_low}</p>
                     <div className="buttons">
@@ -73,15 +76,15 @@ const StockDetailA = ({ symbol, detailType, isWinner }) => {
                         {visibleCharts[stockA.symbol] ? 'Hide Chart' : 'View Chart'}
                       </button>
                     </div>
-                    {visibleCharts[stockA.symbol] && (
+                    {visibleCharts[stockB.symbol] && (
                         <TradingViewMiniWidget
-                            symbol={stockA.symbol}
+                            symbol={stockB.symbol}
                             containerId={`tradingview_widget_${detailType}_${stockA.symbol}`}
                         />
                     )}
                 </div>
             ) : (
-                <p>No stock details available for {symbol}. Please try again and enter a valid symbol.</p>
+                <p>No stock details available for {symbol}. Enter a valid symbol and fetch the details.</p>
             )}
         </div>
     );
