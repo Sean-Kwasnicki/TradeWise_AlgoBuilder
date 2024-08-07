7aac43fce8c9c0e1aac29a73fc4b0039c79194e2
Sean <Kwasnicki.Sean@gmail.com>
2024-08-07 12:57:29 -0700

    update landing page

--- react-vite/src/components/LandingPage/LandingPage.jsx
+++ react-vite/src/components/LandingPage/LandingPage.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
-import tradewise_logo from '../../../../images/tradewise_logo.jpg';
+import tradewise_logo from '../../../../images/tradewise_logo4.png';
 import './LandingPage.css';
 import { getAllPortfoliosThunk, getPortfolioStocksThunk } from '../../redux/portfolio';
 import { getAllWatchlistsThunk, getWatchlistStocksThunk } from '../../redux/watchlist';
@@ -13,7 +13,6 @@ const LandingPage = () => {
             <div className="logo-container">
                 <img src={tradewise_logo} alt="TradeWise Logo" className="logo" />
             </div>
-            <div className="info-links">TradeWise</div>
         </div>
     );
 };
