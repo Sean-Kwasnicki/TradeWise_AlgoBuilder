485d564cde711f6e7add700ed12a9d10758b6233
Sean <Kwasnicki.Sean@gmail.com>
2024-08-12 20:40:36 -0700

    updating algo librbarry css to make it flex

--- react-vite/src/components/SmartChart/AlgoChart.jsx
+++ react-vite/src/components/SmartChart/AlgoChart.jsx
@@ -83,8 +83,8 @@ const AlgoTradingViewWidget = ({ symbol, studies }) => {
   }, [studies]);
 
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}>
-      <div id="tradingview_widget" style={{ height: '800px', width: '700px', margin: '10px' }} />
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '700px', width: '100%' }}>
+      <div id="tradingview_widget" style={{ height: '100%', width: '100%', margin: '10px' }} />
     </div>
   );
 };
